aeea3e524a328d9b6eaa476a57c3e5a1e699d770
Suschenko Roman <55081468+rsushe@users.noreply.github.com>
2024-05-22 11:13:31 +0300

    add frontend (#5)

---
+++ frontend/app/src/main/java/com/smart/house/frontend/theme/Theme.kt
@@ -0,0 +1,50 @@
+package com.smart.house.frontend.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+val Yellow = Color(0xFFFFC107)
+val Pink = Color(0xffd92086)
+
+private val DarkColorScheme = darkColors (
+    primary = Color.Gray,
+    secondary = Pink,
+    background = Color.Black,
+    surface = Color.Black,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White,
+)
+
+private val LightColorScheme = lightColors(
+    primary = Color.Gray,
+    secondary = Pink,
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+)
+
+@Composable
+fun SmartHouseTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
+    }
+
+    MaterialTheme(
+        colors = colors,
+        content = content
+    )
+}
