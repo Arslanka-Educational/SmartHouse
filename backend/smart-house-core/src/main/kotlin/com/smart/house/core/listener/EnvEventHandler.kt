18e2e2676fb7da3e98382af7c05ff8d3a5531541
maksgirya <girya@lanit.ru>
2024-05-23 23:39:30 +0300

    rediska

---
+++ backend/smart-house-core/src/main/kotlin/com/smart/house/core/listener/EnvEventHandler.kt
@@ -0,0 +1,48 @@
+package com.dacha.core.listener
+
+import com.smart.house.db.repository.DeviceRepository
+import com.smart.house.model.DeviceType
+import com.smart.house.model.EnvStateEvent
+import com.smart.house.model.LogSaveRequest
+import kotlinx.coroutines.reactor.awaitSingle
+import org.redisson.api.RedissonReactiveClient
+import java.time.Instant
+
+
+const val LOG_EVENT_QUEUE = "logs"
+const val USER_LOGIN_STUB = "testovy"
+
+class EnvEventHandler(
+    private val redisClient: RedissonReactiveClient,
+) {
+
+    private val deviceRepository = DeviceRepository()
+
+    suspend fun handle(event: EnvStateEvent) {
+        val devices = deviceRepository.findDevicesInHome(event.houseId)
+        for (device in devices) {
+            if (device.type == DeviceType.MANUAL && device.eventType.name != event.envType) {
+                continue
+            }
+
+            val triggerAmount: Int = device.triggerAmount ?: continue
+
+            if ((event.amount >= triggerAmount && !device.state) || (event.amount < triggerAmount && device.state)) {
+                val newState = device.state.not()
+                deviceRepository.updateDeviceState(device.id, newState)
+
+                redisClient.getTopic(LOG_EVENT_QUEUE).publish(
+                    LogSaveRequest(
+                        userLogin = USER_LOGIN_STUB,
+                        logTime = Instant.now(),
+                        houseId = event.houseId,
+                        roomId = device.roomId,
+                        deviceId = device.id,
+                        newState = newState,
+                        message = "Changed device state from ${newState.not()} to $newState"
+                    )
+                ).awaitSingle()
+            }
+        }
+    }
+}
---
+++ backend/smart-house-core/src/main/kotlin/com/smart/house/core/listener/RedisConfiguration.kt
@@ -0,0 +1,50 @@
+package com.dacha.core.listener
+
+import com.smart.house.model.EnvStateEvent
+import io.ktor.server.application.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.reactor.awaitSingle
+import org.redisson.Redisson
+import org.redisson.api.RedissonReactiveClient
+import org.redisson.config.Config
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+
+lateinit var envEventHandler: EnvEventHandler
+lateinit var redisClient: RedissonReactiveClient
+
+val IODispatcher = Dispatchers.IO
+val coroutineScope = CoroutineScope(IODispatcher)
+
+const val ENV_EVENT_QUEUE = "env_state_events"
+const val CHUNK_SIZE = 100
+
+fun Application.configureScheduler() {
+    val config = Config()
+    config.useSingleServer().address = environment.config.property("storage.redis.url").getString()
+
+    redisClient = Redisson.create(config).reactive()
+
+    envEventHandler = EnvEventHandler(redisClient)
+
+    val executor: ScheduledExecutorService = Executors.newScheduledThreadPool(10)
+
+    executor.scheduleAtFixedRate(
+        {
+            coroutineScope.launch {
+                val events: List<EnvStateEvent> = redisClient.getQueue<EnvStateEvent>(ENV_EVENT_QUEUE).poll(CHUNK_SIZE)
+                    .awaitSingle()
+
+                events.forEach {
+                    envEventHandler.handle(it)
+                }
+            }
+        },
+        1000,
+        100,
+        TimeUnit.MILLISECONDS
+    )
+}
